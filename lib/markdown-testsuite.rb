7e26a05ffc3bc43e55c77451158c0a31c4472f91
Dave Kinkead <dave@kinkead.com.au>
2014-04-06 21:40:10 +0200

    Add Markdown::Testsuite#list

--- lib/markdown-testsuite.rb
+++ lib/markdown-testsuite.rb
@@ -3,8 +3,23 @@ require "markdown/version"
 module Markdown
   module Testsuite
     
-    # Public. Lists all core tests
-    def self.list
+    # Public. Lists all core tests and extensions
+    def self.list(filter_by=:core)
+      tests = []
+      if filter_by == :extensions
+        Dir.glob('specification/extensions/*').each do |file|
+          if File.directory? file
+            file[/specification\/extensions\/(.+?)$/]
+            tests << $1
+          end
+        end        
+      else
+        Dir.glob('specification/*.md').each do |file|
+          file[/specification\/(.+?).md/]
+          tests << $1
+        end
+      end
+      tests
     end
     
   end
--- specs/api_spec.rb
+++ specs/api_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
 
 describe Markdown::Testsuite do
-  describe "Public API" do
+  describe "Public API" do  
     it "lists core tests" do
       Markdown::Testsuite.must_respond_to :list
+      Markdown::Testsuite.list.must_be_kind_of Array
+      Markdown::Testsuite.list.include?('2-paragraphs-hard-return-spaces').must_equal true
+      Markdown::Testsuite.list.include?('unordered-list-with-indented-content').must_equal true
+    end
+    
+    it "lists extentions" do
+      Markdown::Testsuite.list(:extensions).must_be_kind_of Array
+      Markdown::Testsuite.list(:extensions).include?('kramdown').must_equal true
+      Markdown::Testsuite.list(:extensions).include?('pandoc').must_equal true
     end
   end
 end
