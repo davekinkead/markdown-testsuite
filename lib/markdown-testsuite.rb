024e5f8ca77bf98b21f9652a6a4dae5382b8f4eb
Dave Kinkead <dave@kinkead.com.au>
2014-04-07 06:50:57 +0200

    Update API documentation

--- lib/markdown-testsuite.rb
+++ lib/markdown-testsuite.rb
@@ -3,7 +3,22 @@ require "markdown/testsuite/version"
 module Markdown
   module Testsuite
         
-    # Public. Lists all core tests and extensions
+    # Public: Lists all available tests or extensions
+    #
+    # filter_by - The extension as a Symbol
+    #
+    # Examples
+    #
+    #   Markdown::Testsuite.list
+    #   # => ["2-paragraphs-hard-return-spaces", "2-paragraphs-hard-return", ...]
+    #
+    #   Markdown::Testsuite.list :extensions
+    #   # => ["gfm", "kramdown", "multimarkdown", "pandoc", "redcarpet"]
+    #
+    #   Markdown::Testsuite.list :kramdown
+    #   # => ["fenced-code-block", "utf8"]
+    #    
+    # Returns the an Array.
     def self.list(filter_by=:core)
       tests = []
       
@@ -33,7 +48,21 @@ module Markdown
       tests
     end
     
-    # Public. Creates a test object for comparison
+    # Public: Creates a test object for a rule
+    #
+    # rule - A String naming the rule
+    # ext  - A Symbol naming the extension
+    #
+    # Examples
+    #
+    #   Markdown::Testsuite.test 'img-title'
+    #   # => #<Markdown::Testsuite::Test:0x007fc633986240
+    #
+    #   Markdown::Testsuite.test 'uf8', :gfm
+    #   # => #<Markdown::Testsuite::Test:0x007fc553986240
+    #
+    #    
+    # Returns a Test object.
     def self.test(rule, ext=nil)
       Markdown::Testsuite::Test.new rule, ext
     end
